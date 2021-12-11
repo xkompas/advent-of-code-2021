3a4b8580a87a89912a1db285e4750b19523c3596
xkompas <3908963+xkompas@users.noreply.github.com>
2021-12-11 14:02:55 +0100

    Add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,16 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.zipWithNext().filter { it.first < it.second }.count()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.windowed(3).map { it.sum() }.zipWithNext().filter { it.first < it.second }
+            .count()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
